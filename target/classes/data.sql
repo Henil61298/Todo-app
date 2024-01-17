86f49949e9aacca909c41d07acf4962f4c646b9e
Henil61298 <44196527+Henil61298@users.noreply.github.com>
2024-01-17 23:11:12 +0530

    Add files via upload

---
+++ target/classes/data.sql
@@ -0,0 +1,11 @@
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'Henil', 'Learn Spring', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'abc', 'Learn to cook', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'Henil', 'Learn Springboot', CURRENT_DATE(), false);
+
+insert into todo(ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'Henil', 'Learn Azure', CURRENT_DATE(), false);
